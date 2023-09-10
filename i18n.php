9d118a6a8d08f0bb0ab43d0be7282ee58098fd43
Никита <52201713+JoricJim@users.noreply.github.com>
2023-09-10 23:38:18 +0300

    Upload

---
+++ i18n.php
@@ -0,0 +1,272 @@
+<?php
+
+$i = [
+    'wg_title'             => [
+        'en' => 'Wireguard',
+        'ru' => 'WireGuard',
+    ],
+    'sh_title'             => [
+        'en' => 'Shadowsocks',
+        'ru' => 'ShadowSocks',
+    ],
+    'ad_title'             => [
+        'en' => 'AdGuard',
+        'ru' => 'AdGuard',
+    ],
+    'config'               => [
+        'en' => 'config',
+        'ru' => 'Настройки',
+    ],
+    'pac'                  => [
+        'en' => 'PAC',
+        'ru' => 'Прокси',
+    ],
+    'chat'                 => [
+        'en' => 'discussion group',
+        'ru' => 'Тех. поддержка',
+    ],
+    'back'                 => [
+        'en' => 'back',
+        'ru' => 'Назад',
+    ],
+    'import'               => [
+        'en' => 'import',
+        'ru' => 'Импорт',
+    ],
+    'export'               => [
+        'en' => 'export',
+        'ru' => 'Экспорт',
+    ],
+    'delete'               => [
+        'en' => 'delete',
+        'ru' => 'Удалить',
+    ],
+    'add'                  => [
+        'en' => 'add',
+        'ru' => 'Добавить',
+    ],
+    'donate'               => [
+        'en' => 'donate',
+        'ru' => 'Донат',
+    ],
+    'admin'                => [
+        'en' => 'admin',
+        'ru' => 'администратора',
+    ],
+    'menu'                 => [
+        'en' => 'Menu',
+        'ru' => 'Меню',
+    ],
+    'update status'        => [
+        'en' => 'update status',
+        'ru' => 'Обновить',
+    ],
+    'add peer'             => [
+        'en' => 'add peer',
+        'ru' => 'Добавить клиента',
+    ],
+    'all traffic'          => [
+        'en' => 'all traffic',
+        'ru' => 'Весь трафик',
+    ],
+    'subnet'               => [
+        'en' => 'subnet',
+        'ru' => 'Подсеть',
+    ],
+    'proxy ip'             => [
+        'en' => 'proxy ip',
+        'ru' => 'Прокси',
+    ],
+    'rename'               => [
+        'en' => 'rename',
+        'ru' => 'Изменить имя',
+    ],
+    'show QR'              => [
+        'en' => 'show QR',
+        'ru' => 'Показать QR',
+    ],
+    'download config'      => [
+        'en' => 'download config',
+        'ru' => 'Скачать',
+    ],
+    'install domain'       => [
+        'en' => 'install domain',
+        'ru' => 'Установить домен',
+    ],
+    'renew SSL'            => [
+        'en' => 'renew SSL',
+        'ru' => 'Обновить SSL',
+    ],
+    'delete SSL'           => [
+        'en' => 'delete SSL',
+        'ru' => 'Удалить SSL',
+    ],
+    'Letsencrypt SSL'      => [
+        'en' => 'Let’s Encrypt',
+        'ru' => 'Let’s Encrypt',
+    ],
+    'Self SSL'             => [
+        'en' => 'Self SSL',
+        'ru' => 'Собственный SSL',
+    ],
+    'change password'      => [
+        'en' => 'change password',
+        'ru' => 'Изменить пароль',
+    ],
+    'reset settings'       => [
+        'en' => 'reset settings',
+        'ru' => 'Сбросить параметры',
+    ],
+    'add upstream'         => [
+        'en' => 'add upstream',
+        'ru' => 'Добавить DNS',
+    ],
+    'check DNS'            => [
+        'en' => 'check DNS',
+        'ru' => 'Проверить DNS',
+    ],
+    'update'               => [
+        'en' => 'update',
+        'ru' => 'Обновить',
+    ],
+    'self list'            => [
+        'en' => 'self list',
+        'ru' => 'Свой список',
+    ],
+    'reverse list'         => [
+        'en' => 'reverse list',
+        'ru' => 'Обратный список',
+    ],
+    'subzones'             => [
+        'en' => 'subzones',
+        'ru' => 'Поддомены',
+    ],
+    'check url'            => [
+        'en' => 'check url',
+        'ru' => 'Проверить домен',
+    ],
+    'blacklist exclude'    => [
+        'en' => 'blacklist exclude',
+        'ru' => 'Исключения РКН',
+    ],
+    'lang'                 => [
+        'en' => 'language',
+        'ru' => 'Язык',
+    ],
+    'adguard web'          => [
+        'en' => 'web interface',
+        'ru' => 'вебморда адгварда',
+    ],
+    'domain explain'       => [
+        'en' => 'Some clients require a valid certificate when connecting, such as windows 11 DoH or ShadowSocks Android (PAC url), this requires a domain',
+        'ru' => 'Меню - Настройки',
+    ],
+    'self list explain'    => [
+        'en' => ' - domains that will work through a proxy',
+        'ru' => ' - домены, которые будут работать через прокси',
+    ],
+    'reverse list explain' => [
+        'en' => ' - domains that will work without a proxy, all others through a proxy',
+        'ru' => ' - домены которые будут работать без прокси, все остальные через прокси',
+    ],
+    'timer'                => [
+        'en' => 'set timer',
+        'ru' => 'Время действия',
+    ],
+    'reset nginx'          => [
+        'en' => 'reset nginx',
+        'ru' => 'сбросить nginx',
+    ],
+    'set internal dns'     => [
+        'en' => 'set internal dns',
+        'ru' => 'Изменить DNS',
+    ],
+    'delete internal dns'  => [
+        'en' => 'delete internal dns',
+        'ru' => 'Изменить DNS',
+    ],
+    'on'  => [
+        'en' => '🟢',
+        'ru' => 'Вкл.',
+    ],
+    'off'  => [
+        'en' => '🔴',
+        'ru' => 'Выкл.',
+    ],
+    'torrent'  => [
+        'en' => 'torrents',
+        'ru' => 'торренты',
+    ],
+    'blinkmenuon'  => [
+        'en' => 'blinkmenu on',
+        'ru' => 'Вкл. меню',
+    ],
+    'blinkmenuoff'  => [
+        'en' => 'blinkmenu off',
+        'ru' => 'Выкл. меню',
+    ],
+    'listSubnet'  => [
+        'en' => 'listSubnet',
+        'ru' => 'Подсети',
+    ],
+    'AllowedIPs'  => [
+        'en' => 'AllowedIPs',
+        'ru' => 'Разрешенные IP',
+    ],
+    'calc'  => [
+        'en' => 'calc CIDR',
+        'ru' => 'Маршрутизация ',
+    ],
+    'mtproto'  => [
+        'en' => 'MTProto',
+        'ru' => 'MTProto',
+    ],
+    'generateSecret'  => [
+        'en' => 'generateSecret',
+        'ru' => 'Сгенерировать ключ',
+    ],
+    'setSecret'  => [
+        'en' => 'setSecret',
+        'ru' => 'Установить свой ключ',
+    ],
+    'defaultDNS'  => [
+        'en' => 'defaultDNS',
+        'ru' => 'Изменить DNS по умолчанию',
+    ],
+    'debug'  => [
+        'en' => 'debug mode',
+        'ru' => 'Отладка',
+    ],
+    'backup'  => [
+        'en' => 'backup',
+        'ru' => 'BackUp',
+    ],
+    'logs'  => [
+        'en' => 'logs',
+        'ru' => 'Логи',
+    ],
+    'clear'  => [
+        'en' => 'clear',
+        'ru' => 'Очисить',
+    ],
+    'xray'  => [
+        'en' => 'XTLS',
+        'ru' => 'XTLS',
+    ],
+    'geodb'  => [
+        'en' => 'GeoIP',
+        'ru' => 'GeoIP',
+    ],
+    'changeFakeDomain'  => [
+        'en' => 'changeFakeDomain',
+        'ru' => 'Установить фейковый домен',
+    ],
+    'page'  => [
+        'en' => 'pagination',
+        'ru' => 'Лимит страниц',
+    ],
+    'exchange'  => [
+        'en' => 'open ip',
+        'ru' => 'обмен',
+    ],
+];
